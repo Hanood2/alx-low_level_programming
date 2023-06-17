9c893e0f2242a795c19dccb34d6fe098b49bb857
Hanood2 <133160754+Hanood2@users.noreply.github.com>
2023-06-17 15:47:03 -0700

    jbgc

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,17 +11,17 @@
 */
 int main(void)
 {
-int num;
+ int num;
 
-num = '0';
-while (num <= '9')
-{
-putchar(num);
-	if (num != '9')
-		putchar(',');
-		putchar(' ');
-	num++;
-}
-putchar('\n');
-return (0);
+ num = '0';
+ while (num <= '9')
+ {
+ putchar(num);
+  if (num != '9')
+  putchar(',');
+  putchar(' ');
+ num++;
+ }
+ putchar('\n');
+ return (0);
 }
