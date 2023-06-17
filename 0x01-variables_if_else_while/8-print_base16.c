91f31ce248254f63c8e0d8ebae64d36e5c96de56
Hanood2 <133160754+Hanood2@users.noreply.github.com>
2023-06-17 12:42:05 -0700

    nhv

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'prints all the numbers of base 16 in lowercase'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+int num;
+
+ch = 'a';
+num = '0';
+while (num <= '9')
+{
+putchar(num);
+num++;
+}
+while (ch <= 'f')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
