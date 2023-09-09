138495261cf3e082c0385b63690dbd9a7e89dd82
Hanood2 <133160754+Hanood2@user>
2023-09-08 18:54:11 -0700

    ffgd

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,44 @@
 #include <stdlib.h>
-#include "holberton.h"
 
 /**
- * string_nconcat - fuction that concats two strings but with s2 by n amount.
- * @s1: base sting to concat to. `Null` will return as empty.
- * @s2: string to copy from. 'NULL' will return as empty string.
- * @n: amount of s2 to copy.
- * Return: pointer to allocated memory.
+ * string_nconcat - Concatenate two strings using n amount of s2
+ * @s1: First string
+ * @s2: String to add to end of s1
+ * @n: Amount of s2 to add to s1
+ *
+ * Return: pointer to new area in memory, NULL if it fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int i, a, b;
+	char *nstr, *empt;
+	unsigned int i, len, j;
+	unsigned int size;
 
-	a = b = 0;
-	if (s1)
-		for (; s1[a]; a++)
-			;
-	if (s2)
-		for (; s2[b]; b++)
-			;
-	if (!(n >= b))
-		b = n;
-
-	str = malloc(sizeof(char) * (a + b) + 1);
-	if (!str)
+	len = 0;
+	empt = "";
+	if (s1 == NULL)
+		s1 = empt;
+	if (s2 == NULL)
+		s2 = empt;
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*nstr);
+	nstr = malloc(size + 1);
+	if (nstr == NULL)
 		return (NULL);
-
-	for (i = 0; i < (a + b); i++)
+	i = 0;
+	while (i < size && s1[i] != '\0')
 	{
-		if (i < a)
-			str[i] = s1[i];
-		else
-			str[i] = s2[i - a];
+		nstr[i] = s1[i];
+		i++;
 	}
-
-	str[i] = '\0';
-
-	return (str);
+	j = 0;
+	while (i < size && s2[j] != '\0')
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	nstr[i] = '\0';
+	return (nstr);
 }
-
